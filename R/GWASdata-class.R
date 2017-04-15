72871c76f1cf386247c619eeb8f0b5a44731879b
alfonso zbook <zbook@reyesaguayo.com>
2017-04-14 19:45:42 -0500

    add constructor for GWASdata

--- R/GWASdata-class.R
+++ R/GWASdata-class.R
@@ -1,6 +1,7 @@
 #' Class
 #'
 #' @importFrom RSQLite dbConnect dbListTables dbGetQuery dbDisconnect
+#' @importFrom RNetCDF open.nc read.nc
 #'
 setClass("GWASdata", slots = c(datapath = "character",
                                dataconn = "list",
@@ -10,15 +11,38 @@ setClass("GWASdata", slots = c(datapath = "character",
                                ncol = "integer"
                                ))
 
+
+GWASdataRead <- function(datapath, metadatapath) {
+    # open the SQLite connection
+    #mydb <- dbConnect(RSQLite::SQLite(), "./inst/extdata/small_metadata.sqlite")
+    mydb <- dbConnect(RSQLite::SQLite(), metadatapath)
+    dbListTables(mydb)
+    subjects <- dbGetQuery(mydb, "SELECT * FROM subjects")
+    snps <- dbGetQuery(mydb, "SELECT * FROM snps")
+    dbDisconnect(mydb)
+
+    # open the netcdf connection
+    nc <- open.nc(datapath)
+    print(nc)
+    nc_data <- read.nc(nc)
+    mytemp <- as.data.frame(nc_data$temperature)
+
+    return(list(subjects, snps, mytemp))
+}
+
+
+
 GWASdata <- function(datapath, metadatapath) {
 
-  #mydb <- dbConnect(RSQLite::SQLite(), "./inst/extdata/small_metadata.sqlite")
-  mydb <- dbConnect(RSQLite::SQLite(), metadatapath)
-  dbListTables(mydb)
-  subjects <- dbGetQuery(mydb, "SELECT * FROM subjects")
-  snps <- dbGetQuery(mydb, "SELECT * FROM snps")
-  dbDisconnect(mydb)
+    dataconn <- open.nc(datapath)
+    class(dataconn) <- NULL
+    dataconn <- list(dataconn)
+    nrow <- 1L
+    ncol <- 1L
+    metadataconn <- dbConnect(RSQLite::SQLite(), metadatapath)
 
-
-  return(list(subjects, snps))
+    new("GWASdata", datapath = datapath, dataconn = dataconn,
+        metadatapath = metadatapath, metadataconn = metadataconn,
+        nrow = nrow, ncol = ncol)
+    # dataconn
 }
